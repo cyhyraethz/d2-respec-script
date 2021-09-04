687156c97cea2c39f9b946b4659027e8851b58ac
Dylan Premo <cyhyraethz@gmail.com>
2021-09-03 21:25:40 -0700

    Modularize code for easier maintenance

--- respec.js
+++ respec.js
@@ -5,19 +5,20 @@ const fs = require('fs');
 
 const args = process.argv;
 
-const unspentOffset = 562; // must be set to 255 for unspent stat and skill points to show up
-const newStatsOffset = 581; // number of unspent stat points
-const newSkillOffset = 585; // number of unspent skill points
-const firstSkillOffset = 619; // first class skill
-const lastSkillOffset = 648; // last class skill
+const unspentOffset = 562;
+const newStatsOffset = 581;
+const newSkillOffset = 585;
+const firstSkillOffset = 619;
+const lastSkillOffset = 648;
+
+let unspentStats;
+let unspentSkills;
 
 const getValue = (buffer, offset) => {
   return buffer[offset].toString(16).toUpperCase();
 };
 
-const resetSkills = (args, buffer) => {
-  let unspentStats = 0;
-  let unspentSkills = 0;
+const getUnspent = (buffer) => {
   switch (getValue(buffer, unspentOffset)) {
     case 'DF':
       unspentStats = parseInt(getValue(buffer, newStatsOffset), 16);
@@ -30,23 +31,35 @@ const resetSkills = (args, buffer) => {
       unspentSkills = parseInt(getValue(buffer, newSkillOffset), 16);
       break;
   }
+};
+
+const addToBuffer = (buffer) => {
   if (!unspentStats) {
-    let startBuffer = buffer.slice(0, newStatsOffset);
-    let endBuffer = buffer.slice(newStatsOffset);
+    const startBuffer = buffer.slice(0, newStatsOffset);
+    const endBuffer = buffer.slice(newStatsOffset);
     buffer = Buffer.concat([startBuffer, Buffer.alloc(4), endBuffer]);
   }
   if (!unspentSkills) {
-    let startBuffer = buffer.slice(0, newSkillOffset);
-    let endBuffer = buffer.slice(newSkillOffset);
+    const startBuffer = buffer.slice(0, newSkillOffset);
+    const endBuffer = buffer.slice(newSkillOffset);
     buffer = Buffer.concat([startBuffer, Buffer.alloc(4), endBuffer]);
   }
+  buffer[unspentOffset] = parseInt('FF', 16);
+};
+
+const resetSkills = (buffer) => {
   let unassignedSkills = 0;
   for (let i = firstSkillOffset; i <= lastSkillOffset; i++) {
     unassignedSkills += parseInt(getValue(buffer, i), 16);
     buffer[i] = parseInt('0', 16);
   }
   buffer[newSkillOffset] = unspentSkills + unassignedSkills;
-  buffer[unspentOffset] = parseInt('FF', 16); // set to 255 to enable unspent stat and skill points
+};
+
+const resetPoints = (args, buffer) => {
+  getUnspent(buffer);
+  addToBuffer(buffer);
+  resetSkills(buffer);
   fs.writeFile(args[2], buffer, (error) => {
     if (error) {
       console.log(error);
@@ -60,6 +73,6 @@ fs.readFile(args[2], (error, buffer) => {
   if (error) {
     return console.log(error);
   } else {
-    resetSkills(args, buffer);
+    resetPoints(args, buffer);
   }
 });
