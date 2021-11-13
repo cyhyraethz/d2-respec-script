89b145f2ca7e28ef4aa204951da687d44349e9a2
Dylan Premo <cyhyraethz@gmail.com>
2021-11-12 23:31:13 -0800

    Refactor respec script

--- respec.js
+++ respec.js
@@ -16,24 +16,23 @@ const unspentOffset = 562; // set to 255 to enable unspent skill points and attr
 const newStatsOffset = 581; // number of unspent attribute points
 const newSkillOffset = 585; // number of unspent skill points
 
-const maxStatsOffsets = [610, 594, 602]; // stamina, life, mana
-const currentStatsOffsets = [606, 590, 598]; // stamina, life, mana
+const maxStatsOffsets = { stamina: 610, life: 594, mana: 602 };
+const currentStatsOffsets = { stamina: 606, life: 590, mana: 598 };
 
-const attributeOffsets = [565, 573, 577, 569]; // strength, dexterity, vitality, energy
+const attributeOffsets = { strength: 565, dexterity: 573, vitality: 577, energy: 569 };
 
-const fractionalLifeOffsets = [589, 593]; // used to calculate life gain
-const fractionalManaOffsets = [597, 601]; // used to calculate mana gain
-const fractionalStaminaOffsets = [605, 609]; // used to calculate stamina gain
+const fractionalOffsets = { stamina: [605, 609], life: [589, 593], mana: [597, 601] };
+
+const questOffsets = { goldenBird: [181, 277, 373], lamEsenTome: [175, 271, 367] };
 
 const startingAttributes = {
-  // strength, dexterity, vitality, energy
-  amazon: [20, 25, 20, 15],
-  sorceress: [10, 25, 10, 35],
-  necromancer: [15, 25, 15, 25],
-  paladin: [25, 20, 25, 15],
-  barbarian: [30, 20, 25, 10],
-  druid: [15, 20, 25, 20],
-  assassin: [20, 20, 20, 25],
+  amazon: { strength: 20, dexterity: 25, vitality: 20, energy: 15 },
+  sorceress: { strength: 10, dexterity: 25, vitality: 10, energy: 35 },
+  necromancer: { strength: 15, dexterity: 25, vitality: 15, energy: 25 },
+  paladin: { strength: 25, dexterity: 20, vitality: 25, energy: 15 },
+  barbarian: { strength: 30, dexterity: 20, vitality: 25, energy: 10 },
+  druid: { strength: 15, dexterity: 20, vitality: 25, energy: 20 },
+  assassin: { strength: 20, dexterity: 20, vitality: 20, energy: 25 },
 };
 
 const startingStats = {
@@ -56,24 +55,13 @@ const statsPerLevel = {
   assassin: { stamina: 1.25, life: 2, mana: 1.5 },
 };
 
-const classNumber = [
-  'amazon',
-  'sorceress',
-  'necromancer',
-  'paladin',
-  'barbarian',
-  'druid',
-  'assassin',
-];
-
-const maxStats = [0, 0, 0];
+const classNumber = ['amazon', 'sorceress', 'necromancer', 'paladin', 'barbarian', 'druid', 'assassin'];
 
 let level; // character level
 let charClass; // character class
 let unspentSkills; // unspent skills
-let unspentAttributes; // unspent attributes
-let firstSkillOffset; // address of first class skill
 let lastSkillOffset; // address of last class skill
+let firstSkillOffset; // address of first class skill
 
 const getValue = (offset, size, buffer) => {
   let value = '';
@@ -102,74 +90,25 @@ const setValue = (value, offset, size, buffer) => {
   return buffer;
 };
 
-const getStats = (buffer) => {
+const setGlobals = (buffer) => {
   level = buffer[levelOffset];
-  for (let i = 0; i < maxStats.length; i++) {
-    maxStats[i] = getValue(maxStatsOffsets[i], bufferSize, buffer);
-  }
-  return buffer;
+  charClass = classNumber[buffer[classOffset]];
+  lastSkillOffset = buffer.indexOf('4A4D', 0, 'hex') - 1;
+  firstSkillOffset = buffer.indexOf('6966', 0, 'hex') + 2;
+  if (buffer[unspentOffset] > 238) unspentSkills = getValue(newSkillOffset, bufferSize, buffer);
 };
 
 const setStats = (buffer) => {
-  let fractionalLife = 0;
-  let fractionalMana = 0;
-  let fractionalStamina = 0;
-  let stamina =
-    startingStats[charClass]['stamina'] +
-    statsPerLevel[charClass]['stamina'] * (level - 1);
-  let life =
-    startingStats[charClass]['life'] +
-    statsPerLevel[charClass]['life'] * (level - 1);
-  let mana =
-    startingStats[charClass]['mana'] +
-    statsPerLevel[charClass]['mana'] * (level - 1);
-  for (let i of [181, 277, 373]) if (buffer[i] === 16) life += 20;
-  fractionalLife = (life % 1) * 256;
-  fractionalMana = (mana % 1) * 256;
-  fractionalStamina = (stamina % 1) * 256;
-  stamina = Math.floor(stamina);
-  life = Math.floor(life);
-  mana = Math.floor(mana);
-  const stats = [stamina, life, mana];
-  for (let i = 0; i < stats.length; i++) {
-    buffer = setValue(stats[i], maxStatsOffsets[i], bufferSize, buffer);
-    buffer = setValue(stats[i], currentStatsOffsets[i], bufferSize, buffer);
-  }
-  for (let i = 0; i < fractionalLifeOffsets.length; i++) {
-    buffer[fractionalLifeOffsets[i]] = fractionalLife;
-  }
-  for (let i = 0; i < fractionalManaOffsets.length; i++) {
-    buffer[fractionalManaOffsets[i]] = fractionalMana;
-  }
-  for (let i = 0; i < fractionalStaminaOffsets.length; i++) {
-    buffer[fractionalStaminaOffsets[i]] = fractionalStamina;
-  }
-  return buffer;
-};
-
-const getSkills = (buffer) => {
-  firstSkillOffset = buffer.indexOf('6966', 0, 'hex') + 2;
-  lastSkillOffset = buffer.indexOf('4A4D', 0, 'hex') - 1;
-  return buffer;
-};
-
-const getClass = (buffer) => {
-  charClass = classNumber[buffer[classOffset]];
-  return buffer;
-};
-
-const getUnspent = (buffer) => {
-  switch (buffer[unspentOffset]) {
-    case 223:
-      unspentAttributes = getValue(newStatsOffset, bufferSize, buffer);
-      break;
-    case 239:
-      unspentSkills = getValue(newSkillOffset, bufferSize, buffer);
-      break;
-    case 255:
-      unspentAttributes = getValue(newStatsOffset, bufferSize, buffer);
-      unspentSkills = getValue(newSkillOffset, bufferSize, buffer);
-      break;
+  const fractionalStats = { stamina: 0, life: 0, mana: 0 };
+  const calculatedStats = { stamina: 0, life: 0, mana: 0 };
+  for (let offset of questOffsets['goldenBird']) if (buffer[offset] === 16) calculatedStats['life'] += 20;
+  for (let stat in calculatedStats) {
+    calculatedStats[stat] += startingStats[charClass][stat] + statsPerLevel[charClass][stat] * (level - 1);
+    fractionalStats[stat] = (calculatedStats[stat] % 1) * 256;
+    calculatedStats[stat] = Math.floor(calculatedStats[stat]);
+    buffer = setValue(calculatedStats[stat], maxStatsOffsets[stat], bufferSize, buffer);
+    buffer = setValue(calculatedStats[stat], currentStatsOffsets[stat], bufferSize, buffer);
+    for (let offset of fractionalOffsets[stat]) buffer[offset] = fractionalStats[stat];
   }
   return buffer;
 };
@@ -202,20 +141,13 @@ const resetSkills = (buffer) => {
 };
 
 const resetAttributes = (buffer) => {
-  const unassigned = [0, 0, 0, 0];
-  const attributes = ['', '', '', ''];
-  for (let i = 0; i < attributeOffsets.length; i++) {
-    for (let j = attributeOffsets[i] + 3; j >= attributeOffsets[i]; j--) {
-      attributes[i] += buffer[j].toString(16).toUpperCase().padStart(2, '0');
-    }
-    attributes[i] = parseInt(attributes[i], 16);
-    unassigned[i] = attributes[i] - startingAttributes[charClass][i];
-    buffer.fill(0, attributeOffsets[i], attributeOffsets[i] + 4);
-    buffer[attributeOffsets[i]] = startingAttributes[charClass][i];
+  let unspentAttributes = 5 * (level - 1);
+  for (let offset of questOffsets['lamEsenTome']) if (buffer[offset] === 16) unspentAttributes += 5;
+  for (let offset in attributeOffsets) {
+    buffer.fill(0, attributeOffsets[offset], attributeOffsets[offset] + 4);
+    buffer[attributeOffsets[offset]] = startingAttributes[charClass][offset];
   }
-  const totalUnassigned =
-    (unspentAttributes || 0) + unassigned.reduce((a, b) => a + b);
-  buffer = setValue(totalUnassigned, newStatsOffset, bufferSize, buffer);
+  buffer = setValue(unspentAttributes, newStatsOffset, bufferSize, buffer);
   buffer = setStats(buffer);
   return buffer;
 };
@@ -225,11 +157,8 @@ fs.readFile(args[2], (error, buffer) => {
     if (error) {
       return console.log(error);
     } else {
+      setGlobals(buffer);
       buffer = addToBuffer(buffer);
-      buffer = getUnspent(buffer);
-      buffer = getClass(buffer);
-      buffer = getStats(buffer);
-      buffer = getSkills(buffer);
       buffer = resetSkills(buffer);
       buffer = resetAttributes(buffer);
       fs.writeFile(args[2], buffer, (error) => {
@@ -241,8 +170,6 @@ fs.readFile(args[2], (error, buffer) => {
       });
     }
   } else {
-    console.log(
-      'Error: Wrong version number. Only works for patch 1.00 - 1.08.'
-    );
+    console.log('Error: Wrong version number. Only works for patch 1.00 - 1.08.');
   }
 });
